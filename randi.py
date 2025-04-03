75ffdadc31a8ae14cd85a178f8393969ce4ed89b
MD7542Ali <tboi77349@gmail.com>
2025-04-03 08:40:23 +0530

    Update randi.py

--- randi.py
+++ randi.py
@@ -7,7 +7,7 @@ def is_bot_running(process_name):
     for process in psutil.process_iter(attrs=['pid', 'name', 'cmdline']):
         try:
             # Check if the cmdline contains the name of the bot (p.py)
-            if process.info['cmdline'] and 'bot.py' in process.info['cmdline']:
+            if process.info['cmdline'] and 'pp.py' in process.info['cmdline']:
                 return True
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             # If the process is not accessible or dead, we skip it
@@ -15,11 +15,11 @@ def is_bot_running(process_name):
     return False
 
 def restart_bot():
-    # Replace 'python3 bot.py' with the actual command to start your bot
-    os.system("python3 bot.py &")  # Start your bot in the background
+    # Replace 'python3 pp.py' with the actual command to start your bot
+    os.system("python3 pp.py &")  # Start your bot in the background
 
 if __name__ == "__main__":
-    bot_process_name = "bot.py"  # The name of your bot's script
+    bot_process_name = "pp.py"  # The name of your bot's script
     while True:
         if not is_bot_running(bot_process_name):
             print(f"Bot is OFF, checking again in 5 seconds.")
